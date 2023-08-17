31483f94132ac16cbd27c4e3ad2650b0139d3dba
yonbo <yonbot0172@gmail.com>
2023-08-17 21:30:40 +0900

    first commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+  <div style="margin-top: 100px">
+    <h2 class="text-muted text-center">No Posts yet.</h2>
+    <p class="text-center">
+      <a href="#" class="text-decoration-none">Create new post</a>
+    </p>
+  </div>
+@endsection
